b6f7bfac70735432f3fd03d4941f8662312d17df
Andrew Lush <andrew.lush@outlook.com>
2025-02-13 11:52:11 +1100

    Day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -3,28 +3,31 @@ use glam::IVec2;
 use std::collections::HashMap;
 
 const DIRECTIONS: [[IVec2; 3]; 8] = [
-    [IVec2::new(0, 1),   IVec2::new(0, 2),   IVec2::new(0, 3)],
-    [IVec2::new(0, -1),  IVec2::new(0, -2),  IVec2::new(0, -3), ],
-    [IVec2::new(1, 1),   IVec2::new(2, 2),   IVec2::new(3, 3)],
-    [IVec2::new(1, -1),  IVec2::new(2, -2),  IVec2::new(3, -3), ],
-    [IVec2::new(-1, 1),  IVec2::new(-2, 2),  IVec2::new(-3, 3), ],
-    [IVec2::new(-1, -1), IVec2::new(-2, -2), IVec2::new(-3, -3), ],
-    [IVec2::new(1, 0),   IVec2::new(2, 0),   IVec2::new(3, 0)],
-    [IVec2::new(-1, 0),  IVec2::new(-2, 0),  IVec2::new(-3, 0),  ],
+    [IVec2::new(0, 1), IVec2::new(0, 2), IVec2::new(0, 3)],
+    [IVec2::new(0, -1), IVec2::new(0, -2), IVec2::new(0, -3)],
+    [IVec2::new(1, 1), IVec2::new(2, 2), IVec2::new(3, 3)],
+    [IVec2::new(1, -1), IVec2::new(2, -2), IVec2::new(3, -3)],
+    [IVec2::new(-1, 1), IVec2::new(-2, 2), IVec2::new(-3, 3)],
+    [IVec2::new(-1, -1), IVec2::new(-2, -2), IVec2::new(-3, -3)],
+    [IVec2::new(1, 0), IVec2::new(2, 0), IVec2::new(3, 0)],
+    [IVec2::new(-1, 0), IVec2::new(-2, 0), IVec2::new(-3, 0)],
 ];
-
+const DIRECTIONS_2: [[IVec2; 2]; 4] = [
+    [IVec2::new(-1, -1), IVec2::new(1, 1)],
+    [IVec2::new(-1, 1), IVec2::new(1, -1)],
+    [IVec2::new(1, 1), IVec2::new(-1, -1)],
+    [IVec2::new(1, -1), IVec2::new(-1, 1)],
+];
 pub fn part_one(input: &str) -> Option<usize> {
     let positions = input
         .lines()
         .enumerate()
         .flat_map(|(y, line)| {
-            line.chars().enumerate().map(move |(x, value)| {
-                (IVec2::new(x as i32, y as i32), value)
-            },
-        )
-        }).collect::<HashMap<IVec2, char>>();
-    
-    println!("Positions: {:?}", positions);
+            line.chars()
+                .enumerate()
+                .map(move |(x, value)| (IVec2::new(x as i32, y as i32), value))
+        })
+        .collect::<HashMap<IVec2, char>>();
 
     let mas = ['M', 'A', 'S'];
 
@@ -37,26 +40,49 @@ pub fn part_one(input: &str) -> Option<usize> {
                 .map(|mas_positions| {
                     mas_positions
                         .iter()
-                        .map(|offset| {
-                            positions
-                                .get(&(position + offset))
-                        })
+                        .map(|offset| positions.get(&(position + offset)))
                         .enumerate()
-                        .all(|(index, value)| {
-                            mas.get(index) == value
-                        })
+                        .all(|(index, value)| mas.get(index) == value)
                 })
                 .filter(|b| *b)
                 .count();
-//=            info!(?position, ?value, count);
+            //=            info!(?position, ?value, count);
             count
         })
         .sum();
     Some(result)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let positions = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, value)| (IVec2::new(x as i32, y as i32), value))
+        })
+        .collect::<HashMap<IVec2, char>>();
+    let mas = ['M', 'S'];
+    let result: usize = positions
+        .iter()
+        .filter(|(_position, value)| **value == 'A')
+        .filter(|(position, _value)| {
+            DIRECTIONS_2
+                .iter()
+                .map(|ms_positions| {
+                    ms_positions
+                        .iter()
+                        .map(|pos| positions.get(&(*position + pos)))
+                        .enumerate()
+                        .all(|(index, value)| mas.get(index) == value)
+                })
+                .filter(|b| *b)
+                .count()
+                == 2
+        })
+        .count();
+    Some(result as u64)
 }
 
 #[cfg(test)]
@@ -71,7 +97,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(9));
     }
 }
